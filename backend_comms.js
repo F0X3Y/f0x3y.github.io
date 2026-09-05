b2ae1d5083fdaf533f5743f266a883205ba12468
m125 <kaman.marcell.2009.10.28@gmail.com>
2026-09-05 14:28:54 +0200

    tutoriale

--- backend_comms.js
+++ backend_comms.js
@@ -49,5 +49,40 @@ async function get_username(url) {
         return await result.text()
     }
 
-    return "Felhasználó"
+    return "Nincs bejelentkezve"
+}
+
+async function get_videos(url,page=1){
+    //lists video names (1 page is 3 videos, pages start from 1)
+    //to be used in to_thumbnail_link() and to_video_link()
+    //only returns videos of currently logged in user
+    //returns list of strings ["","",""]
+    //
+    //magyarul: 
+    // video lista
+    // 1 oldal=3 video de tudom állítani ezt szerveren
+    // csak saját videóidat adja ki (amilyen néven be vagy jelentkezve)
+    // visszadob egy stringekkel teli listát ["név1","név2","valami"]
+    //
+    // Amúgy meg valamiért sokkal könnyeb először angolul leírni utána magyarul nemtom miért
+
+    if (!page){
+        page=1
+    }
+    console.log(page)
+    result=await fetch(url+"videos/"+page,
+        {method:"GET",
+            headers:{"token":get_token()}
+        })
+    return await result.json()
+}
+function to_thumbnail_link(url,video_name){
+    //Turn video names into links to thumbnails (session token in link so it knows who is signed in)
+    // magyarul: videó nevéből csinál thumbnail linket (session token a linkben)
+    return url+"thumbnail/"+video_name+"?token="+get_token()
+}
+function to_video_link(url,video_name){
+    //Turn video names into links to videos (session token in link so it knows who is signed in)
+    // magyarul: videó nevéből csinál video linket (session token a linkben)
+    return url+"video/"+video_name+"?token="+get_token()
 }
